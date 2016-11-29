cfa6fafa7ef83e6417f3bdb827f2a954f0ec27e5
Rahul <rahuljoshi6070@gmail.com>
2016-11-29 20:25:44 +0530

    Finish map functionalities v2.0

--- src/js/app.js
+++ src/js/app.js
@@ -6,38 +6,89 @@ var locations = [
   {location:{lat:19.03648,lng:72.81725},title:'Bandra Worli Sea Link',content: 'It is a cable-stayed bridge with pre-stressed concrete-steel via ducts on either side that links Bandra in the Western Suburbs of Mumbai with Worli in South Mumbai.'}
 ];
 
+var markers = [];
+
 var viewModel = function() {
   var self = this;
-  self.currentArray = ko.observableArray([]);
-  locations.forEach(function(loc) {
-    self.currentArray.push(loc);
-  });
-  console.log(self.currentArray());
+  // self.currentArray = ko.observableArray([]);
+  // locations.forEach(function(loc) {
+  //   self.currentArray.push(loc);
+  // });
 
-  button.addEventListener('click', function() {
-    // div1.classList.toggle('open');
-    self.currentArray().length = 0;
-    console.log(self.currentArray());
-  });
-
-  self.render = ko.computed(function() {
-    this.currentArray().forEach(function(arr){
-      var marker = new google.maps.Marker({
-        position: arr.location,
-        map: map,
-        title: arr.title
-      });
-
-      var infoWindow = new google.maps.InfoWindow({
-        content: arr.content
-      })
-
-      marker.addListener('click',function() {
-        infoWindow.open(map,marker);
-      })
+  locations.forEach(function(arr){
+    var marker = new google.maps.Marker({
+      position: arr.location,
+      map: map,
+      title: arr.title
     });
-  },self);
 
+    marker.infoWindow = new google.maps.InfoWindow({
+      content: arr.content
+    });
+
+    marker.addListener('click',function() {
+      openInfoWindow(marker);//,infoWindow);
+    })
+
+    marker.display = ko.observable(1);
+
+    marker.wikiUrl = wikiUrl1+arr.title+wikiUrl1;
+    $.ajax( {
+      url: marker.wikiUrl,
+      dataType: 'jsonp',
+      success: function(data) {
+          console.log(data);
+      }
+      // clearTimeout(wikiError);
+    });
+
+    markers.push(marker);
+  });
+
+  self.active = function() {
+    self.noneVisible();
+    this.setVisible(true);
+    this.display(1);
+    openInfoWindow(this);
+  }
+
+  var openInfoWindow = function(marker) {
+    markers.forEach(function(marker) {
+      marker.infoWindow.close();
+    });
+    marker.infoWindow.open(map,marker);
+  }
+
+  self.allVisible = function() {
+    markers.forEach(function(marker) {
+      marker.display(1);
+      marker.infoWindow.close();
+      marker.setVisible(true);
+    })
+  }
+
+  self.noneVisible = function() {
+    markers.forEach(function(marker) {
+      marker.setVisible(false);
+    })
+  }
+
+  self.filterVal = "";
+
+  // indexOf ref=http://stackoverflow.com/questions/237104/how-do-i-check-if-an-array-includes-an-object-in-javascript
+  self.filter = function() {
+    markers.forEach(function(marker) {
+      marker.infoWindow.close();
+      if(marker.title.toLowerCase().indexOf(self.filterVal.toLowerCase()) > -1) {
+        marker.setVisible(true);
+        marker.display(1);
+      }else {
+        marker.setVisible(false);
+        marker.display(0);
+        // alert("No matches found!");
+      }
+    })
+  }
 }
 
 //map
@@ -46,16 +97,30 @@ function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 19.067512, lng: 72.879770},
     zoom: 11,
-    disableDefaultUI: true
+    // disableDefaultUI: true
+    mapTypeControl: false   // This option doesn't remove the zoom controls. ref=http://stackoverflow.com/questions/4321606/how-to-remove-a-maptype-from-google-map-using-google-maps-javascript-v3-api
   });
   ko.applyBindings(new viewModel());    //Reference=https://discussions.udacity.com/t/handling-google-maps-in-async-and-fallback/34282#placing-callback
 }
 
 
-// Sliding Side Bar
+// Sliding Side Bar ref=Responsive web development course
 var button = document.querySelector('.filterBtn-svg');
 var div1 = document.querySelector('.div1');
 
 button.addEventListener('click', function() {
   div1.classList.toggle('open');
 });
+
+var textbox = document.getElementById('text');
+var reset = document.querySelector('#reset');
+
+reset.addEventListener('click', function() {
+  textbox.value = " ";
+})
+
+// var wikiError = setTimeout(function(){
+//     $wikiHeader.text("Failed to load wikipedia resources");
+// },8000);
+var wikiUrl1 = "https://en.wikipedia.org/w/api.php?action=opensearch&search=";
+var wikiUrl2 = "&format=json";
